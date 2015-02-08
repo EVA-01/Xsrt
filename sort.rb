ea466bbb595bf3e8d03cdfdbadb71b259b394249
James Anthony Bruno <j.bruno.che@gmail.com>
2015-02-08 17:16:12 -0600

    More supported formats

--- sort.rb
+++ sort.rb
@@ -1,20 +1,20 @@
 require 'pxlsrt'
 require 'fileutils'
 
-jpg = Dir["./*.jpg"]
-png = Dir["./*.png"]
-images = jpg.concat(png)
+formats = ['.png', '.jpg', '.jpeg', '.bmp', '.tiff']
+images = []
+for format in formats
+	images = images.concat(Dir["./*#{format}"])
+end
 Dir.mkdir("./sorted") unless Dir.exists?("./sorted")
 Dir.mkdir("./originals") unless Dir.exists?("./originals")
 for image in images
 	puts Pxlsrt::Helpers.red(image)
 	FileUtils.mv(image, "./originals/#{image}")
-	if File.extname(image) == '.jpg'
-		base = File.basename(image, '.jpg')
+	base = File.basename(image, File.extname(image))
+	unless File.extname(image) == '.png'
 		# ImageMagick
 		system "convert ./originals/#{image} ./originals/#{base}.png"
-	else
-		base = File.basename(image, '.png')
 	end
 	# system "pxlsrt smart ./originals/#{base}.png ./sorted/#{base}.1.png -V -m alpha -r -d -t 200"
 	Pxlsrt::Smart.suite("./originals/#{base}.png", "./sorted/#{base}.1.png", :verbose => true, :method => "alpha", :diagonal => true, :reverse => true, :threshold => 200)
